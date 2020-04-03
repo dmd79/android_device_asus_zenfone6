aa121eadb5747becb1229f00d5e80df659c5c6e7
dmd79 <davidevinavil@gmail.com>
2020-04-03 19:34:18 +0200

    aex-ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,7 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/device.mk \
-    $(LOCAL_DIR)/omni_zenfone6.mk
+    $(LOCAL_DIR)/aosp_zenfone6.mk
 
 COMMON_LUNCH_CHOICES := \
-    omni_zenfone6-user \
-    omni_zenfone6-userdebug \
-    omni_zenfone6-eng
+    aosp_zenfone6-userdebug \
+    aosp_zenfone6-eng
--- aosp_zenfone6.mk
+++ aosp_zenfone6.mk
@@ -22,25 +22,20 @@
 # Sample: This is where we'd set a backup provider if we had one
 # $(call inherit-product, device/sample/products/backup_overlay.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Get the prebuilt list of APNs
-$(call inherit-product, vendor/omni/config/gsm.mk)
+# Boot animation resolution
+TARGET_BOOT_ANIMATION_RES := 1080
 
-# Inherit from the common Open Source product configuration
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
-
-# must be before including omni part
-TARGET_BOOTANIMATION_SIZE := 1080p
-
-# Inherit from our custom product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common AOSP-OMS stuff.
+$(call inherit-product, vendor/aosp/common.mk)
 
 # Inherit from hardware-specific part of the product configuration
 $(call inherit-product, device/asus/zenfone6/device.mk)
 
 # Discard inherited values and use our own instead.
 PRODUCT_DEVICE := zenfone6
-PRODUCT_NAME := omni_zenfone6
+PRODUCT_NAME := aosp_zenfone6
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := ASUS_I01WD
 PRODUCT_MANUFACTURER := asus
@@ -49,12 +44,11 @@ TARGET_DEVICE := WW_I01WD
 PRODUCT_SYSTEM_DEVICE := ASUS_I01WD
 PRODUCT_SYSTEM_NAME := WW_I01WD
 
-OMNI_PRODUCT_PROPERTIES += \
-    ro.build.product=ZS630KL
+# Build info
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE=ZS630KL \
+    PRODUCT_NAME=WW_I01WD
 
-VENDOR_RELEASE := 10/QP1A.190711.005/17.1810.2002.133-0:user/release-keys
 BUILD_FINGERPRINT := asus/WW_I01WD/ASUS_I01WD:$(VENDOR_RELEASE)
-OMNI_BUILD_FINGERPRINT := asus/WW_I01WD/ASUS_I01WD:$(VENDOR_RELEASE)
-OMNI_PRIVATE_BUILD_DESC := "'QP1A.190711.005.WW_Phone-17.1810.2002.133-0'"
 
 PLATFORM_SECURITY_PATCH_OVERRIDE := 2020-02-01
